470a3b2c886b224da762dcacee34f5b570525f4d
Daniel Marin <danmarcab@gmail.com>
2018-09-25 12:30:45 +0100

    upgrade to elm 0.19

--- src/Material/Icons/Internal.elm
+++ src/Material/Icons/Internal.elm
@@ -9,34 +9,34 @@ icon : String -> List (Svg msg) -> Color -> Int -> Svg msg
 icon viewBox children color size =
     let
         stringSize =
-            toString size
+            String.fromInt size
 
         stringColor =
             toRgbaString color
     in
-        Svg.svg
-            [ Svg.Attributes.width stringSize
-            , Svg.Attributes.height stringSize
-            , Svg.Attributes.viewBox viewBox
-            ]
-            [ Svg.g
-                [ Svg.Attributes.fill stringColor ]
-                children
-            ]
+    Svg.svg
+        [ Svg.Attributes.width stringSize
+        , Svg.Attributes.height stringSize
+        , Svg.Attributes.viewBox viewBox
+        ]
+        [ Svg.g
+            [ Svg.Attributes.fill stringColor ]
+            children
+        ]
 
 
 toRgbaString : Color -> String
 toRgbaString color =
     let
         { red, green, blue, alpha } =
-            Color.toRgb color
+            Color.toRgba color
     in
-        "rgba("
-            ++ toString red
-            ++ ","
-            ++ toString green
-            ++ ","
-            ++ toString blue
-            ++ ","
-            ++ toString alpha
-            ++ ")"
+    "rgba("
+        ++ String.fromInt (round (255 * red))
+        ++ ","
+        ++ String.fromInt (round (255 * green))
+        ++ ","
+        ++ String.fromInt (round (255 * blue))
+        ++ ","
+        ++ String.fromFloat alpha
+        ++ ")"
